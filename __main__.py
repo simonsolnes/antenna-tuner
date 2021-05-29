69fb4cdb3014984a8f37192fc82856c0417a40d6
Simon Solnes <simon@solnes.co>
2021-05-29 21:23:23 +0200

    added color support

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 from tkinter import StringVar, messagebox
 from tkinter.constants import W
+from tkinter.colorchooser import askcolor
 import json
 import time
 import sys
@@ -14,6 +15,8 @@ def from_rgb(rgb):
     return "#%02x%02x%02x" % rgb
 
 constants = {
+    'button_color': None,
+    'toggle_button_default_color': from_rgb((230, 230, 230)),
     'red': from_rgb((220, 100, 100)),
     'green': from_rgb((60, 180, 60)),
     'user_settings_path': 'user_settings.json' if sys.platform == 'darwin' else '/Users/Sander/Documents/Simon/antenna-tuner/user_settings.json'
@@ -65,10 +68,21 @@ def update_status():
             set_button_active(nc_button)
 
 def update_settings():
-    for attrs, label, nc_b, no_b in zip(UserSettings().values(), labels, nc_buttons, no_buttons): 
+    for attrs, label, toggle_b, nc_b, no_b in zip(UserSettings().values(), labels, toggle_buttons, nc_buttons, no_buttons): 
         label.config(text=attrs['label'])
         nc_b['text'] = attrs['nc']
         no_b['text'] = attrs['no']
+        if sys.platform != 'darwin':
+            if attrs['color'] != 'none':
+                toggle_b.config(bg=attrs['color'])
+            else:
+                toggle_b.config(bg=constants['toggle_button_default_color'])
+        else:
+            if attrs['color'] != 'none':
+                toggle_b.config(fg=attrs['color'])
+            else:
+                toggle_b.config(fg=from_rgb((255, 255, 255)))
+                
 
 def settings():
     label_config = tk.Toplevel(root)
@@ -78,28 +92,47 @@ def settings():
     label_config_table.pack()
 
     user_settings = UserSettings()
+    print('USER SETTINGS:', user_settings)
 
     label_vars = [StringVar(value=user_settings[str(i+1)]['label']) for i in range(8)]
     nc_vars = [StringVar(value=user_settings[str(i+1)]['nc']) for i in range(8)]
     no_vars = [StringVar(value=user_settings[str(i+1)]['no']) for i in range(8)]
+    colors = [c if (c := user_settings[str(i+1)]['color']) != 'none' else None for i in range(8)]
+    print('COLORS:', colors)
+
+    def create_color_setter(relay):
+        def inner():
+            print('in setter')
+            colors[relay-1] = askcolor(color=colors[relay-1])[1]
+            print(f'color {colors[relay-1]} was set for relay {relay}')
+        return inner
+    def create_color_resetter(relay):
+        def inner():
+            colors[relay-1] = None
+        return inner
 
     tk.Label(label_config_table, text='Relay Label').grid(row=0, column=1)
     tk.Label(label_config_table, text='NC Label').grid(row=0, column=2)
     tk.Label(label_config_table, text='NO Label').grid(row=0, column=3)
+    tk.Label(label_config_table, text='Color').grid(row=0, column=4)
+    tk.Label(label_config_table, text='Color').grid(row=0, column=5)
     for i in range(8):
         tk.Label(label_config_table, text='Relay {}'.format(i + 1)).grid(row=i+1, column=0)
         tk.Entry(label_config_table, textvariable=label_vars[i], width=30).grid(row=i+1, column=1)
         tk.Entry(label_config_table, textvariable=nc_vars[i], width=20).grid(row=i+1, column=2)
         tk.Entry(label_config_table, textvariable=no_vars[i], width=20).grid(row=i+1, column=3)
+        tk.Button(label_config_table, text="Set", command=create_color_setter(i+1)).grid(row=i+1, column=4)
+        tk.Button(label_config_table, text="Reset", command=create_color_resetter(i+1)).grid(row=i+1, column=5)
     
     def cancel():
         pass
 
     def save():
-        for i, (lbl, nc, no) in enumerate(zip(label_vars, nc_vars, no_vars)):
+        for i, (lbl, nc, no, clr) in enumerate(zip(label_vars, nc_vars, no_vars, colors)):
             user_settings[str(i+1)]['label'] = lbl.get()
             user_settings[str(i+1)]['nc'] = nc.get()
             user_settings[str(i+1)]['no'] = no.get()
+            user_settings[str(i+1)]['color'] = clr if clr else 'none'
         user_settings.write()
         update_settings()
 
@@ -160,16 +193,7 @@ for relay_num, row in zip(range(1, 9), range(1, 9)):
     no_button.grid(row=row, column=4, padx=4, pady=4)
     no_buttons.append(no_button)
 
+update_settings()
 relays = RelayArray()
 update_status()
-'''
-def openColorDialog():
-    # display color dialog box
-    color = colorDialog.show()
-    # show the chosen RBG value
-    tkinter.messagebox.showinfo("Notice", "The color you chose is: " + color[1] + "n" + 
-        "R = " + str(color[0][0]) + " G = " + str(color[0][1]) + " B = " + str(color[0][2]))
-colorDialog = tkinter.colorchooser.Chooser(root)
-tk.Button(root, text='color', command=openColorDialog).pack()
-'''
 root.mainloop()
